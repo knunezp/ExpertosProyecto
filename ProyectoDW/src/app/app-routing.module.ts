57ee1657f790d9894e842315ae4ba9338420badf
knunezp <39134437+knunezp@users.noreply.github.com>
2020-09-28 17:59:45 -0600

    agregar componentes

--- ProyectoDW/src/app/app-routing.module.ts
+++ ProyectoDW/src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:"inicio",loadChildren:()=>
+    import("./componentes/inicio/inicio.module").then(m=>m.InicioModule)
+  },
+  {path:"",pathMatch:"full",redirectTo:"inicio"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ProyectoDW/src/app/app.module.ts
+++ ProyectoDW/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { LandingPageComponent } from './componentes/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,

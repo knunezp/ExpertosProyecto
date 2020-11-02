86bab6db9bf9631dfdfc06d07c285fff4875efd7
knunezp <39134437+knunezp@users.noreply.github.com>
2020-11-01 21:08:05 -0600

    nav

--- ProyectoDW/src/app/administracion/admin/admin.component.ts
+++ ProyectoDW/src/app/administracion/admin/admin.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  
+  opened=true;
   constructor() { }
 
   ngOnInit(): void {
     window.scrollTo(0,0);
+  }
+  toggleSidebar(){
+    this.opened=!this.opened;
   }
 
 }
